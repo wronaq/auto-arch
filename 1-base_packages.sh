5b223c0828091056257104df2dd436f88329d2fa
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-03-27 15:22:16 +0100

    feat: base packages

---
+++ 1-base_packages.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo -e "\nInstalling Base System\n"
+
+PKGS=(
+
+    # --- XORG Display Rendering
+        'xorg'                  # Base Package
+        'xorg-apps'             # Xorg apps group
+        'xorg-xinit'            # Xorg init
+        'libx11'                # Xorg client-side
+        'libxinerama'           # Many displays
+        'libxft'                # Basic fonts
+        'webkit2gtk'            # Webkit
+
+    # --- Setup Desktop
+        'picom'                 # Translucent Windows
+        'nitrogen'              # Set Wallpaper
+
+    # --- Networking Setup
+        'wpa_supplicant'            # Key negotiation for WPA wireless networks
+
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+
+echo "INSTALLING dwm, dmenu and st"
+cd suckless/dwm && sudo make clean install
+cd ../dmenu && sudo make clean install
+cd ../st && sudo make clean install
+
+
+echo -e "\nDone!\n"
