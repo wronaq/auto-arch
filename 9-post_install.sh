0638c737e906a1d9bebf90554f0a07e7f94e9a3c
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-08-14 23:14:09 +0200

    Remove redundant files

--- 9-post_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\nFINAL SETUP AND CONFIGURATION"
-
-echo -e "\nCopy config files"
-cp -r configs/. ~/
-
-# set password for sudo
-sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
